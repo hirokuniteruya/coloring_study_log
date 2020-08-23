a5a8176f0a90908b768774a7573d333f7e0614aa
hirokuniteruya <59299465+hirokuniteruya@users.noreply.github.com>
2020-08-23 14:11:11 +0900

    Create coloring_study_log.rb
    gitエラーなったので直接ファイル追加しました

---
+++ coloring_study_log.rb
@@ -0,0 +1,41 @@
+def update_logs(input_subject, input_minutes,study_logs)
+  input_subject.downcase!
+  if study_logs.has_key?(input_subject)
+    study_logs[input_subject] += input_minutes
+  else
+    study_logs[input_subject] = input_minutes
+  end
+end
+
+def print_logs(study_logs)
+  study_logs.each{|subject,minutes|
+    coloring_blocks = ""
+    (minutes/15).times {
+      coloring_blocks << "■"
+    }
+
+    puts "#{subject} #{coloring_blocks}"
+  }
+end
+
+def main_proc
+  study_logs = {}
+  while true do
+    # 変数の初期化
+    input_subject = nil
+    input_minutes = nil
+    puts "※ 入力を終了する場合はexitと入力してください。"
+    puts "学習項目を入力："
+    input_subject = gets.chomp
+    return if input_subject=="exit"
+    puts "学習時間を入力（単位：分）："
+    input_minutes = gets.to_i
+    return if input_minutes=="exit"
+
+    update_logs(input_subject,input_minutes,study_logs)
+    print_logs(study_logs)
+
+  end
+end
+
+main_proc
